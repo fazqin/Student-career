d9617b4b2ad1d53d005b6b12dafa7b4fa85ca7c5
fazqin <cyberpunkcrime@gmail.com>
2026-09-18 17:05:35 +0700

    ubah struktur folder menjadi turborepo

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -10,6 +10,8 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: __dirname,
+  publicDir: path.resolve(__dirname, "public"),
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
