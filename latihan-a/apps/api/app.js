e5d1314d012af8aaef60bbe0d0cfb708a777fb19
afni1011 <afniana17@gmail.com>
2026-09-20 23:48:18 +0700

    Revisi

--- latihan-a/apps/api/app.js
+++ latihan-a/apps/api/app.js
@@ -1,11 +1,6 @@
 const express = require("express");
-
 const companyRoutes = require("./src/routes/company.routes");
-
 const app = express();
-
 app.use(express.json());
-
 app.use("/api", companyRoutes);
-
 module.exports = app;
--- latihan-a/apps/api/src/routes/company.routes.js
+++ latihan-a/apps/api/src/routes/company.routes.js
@@ -1,8 +1,5 @@
 const express = require("express");
-
 const router = express.Router();
-
-
 const companies = [
     {
         id: 1,
@@ -20,38 +17,24 @@ const companies = [
     }
 ];
 
-
 // GET semua perusahaan
 // Endpoint: GET /api/companies
 router.get("/companies", (req, res) => {
-
     res.json(companies);
-
 });
-
 
 // GET perusahaan berdasarkan ID
 // Endpoint: GET /api/companies/:id
 router.get("/companies/:id", (req, res) => {
-
     const id = parseInt(req.params.id, 10);
-
     const company = companies.find((item) => item.id === id);
-
-
     if (company) {
-
         res.json(company);
-
     } else {
-
         res.status(404).json({
             error: "Company Not Found"
         });
-
     }
-
 });
-
 
 module.exports = router;
