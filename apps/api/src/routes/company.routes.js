26de495213b44738f277d9da6dca0dbe017daca3
afni1011 <afniana17@gmail.com>
2026-09-21 19:42:16 +0700

    Revisi API

--- apps/api/src/routes/company.routes.js
+++ apps/api/src/routes/company.routes.js
@@ -1,41 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const companies = [
-    {
-        id: 1,
-        name: "Telkom Indonesia",
-        industry: "Telecommunication",
-        location: "Bandung",
-        website: "https://telkom.co.id"
-    },
-    {
-        id: 2,
-        name: "Tokopedia",
-        industry: "Technology",
-        location: "Jakarta",
-        website: "https://tokopedia.com"
-    }
-];
+const {
+    getAllCompanies,
+    getCompanyById
+} = require("../controllers/company.controller");
 
-// GET semua perusahaan
-// Endpoint: GET /api/companies
-router.get("/companies", (req, res) => {
-    res.json(companies);
-});
+// GET semua company
+// /api/companies
+router.get("/companies", getAllCompanies);
 
-// GET perusahaan berdasarkan ID
-// Endpoint: GET /api/companies/:id
-router.get("/companies/:id", (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const company = companies.find((item) => item.id === id);
-    if (company) {
-        res.json(company);
-    } else {
+// GET company berdasarkan id
+// /api/companies/:id
+router.get("/companies/:id", getCompanyById);
 
-        res.status(404).json({
-            error: "Company Not Found"
-        });
-    }
-});
+
+
 module.exports = router;
