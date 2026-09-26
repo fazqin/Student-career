51032c1eec22982d559a683106dbf60f88fa47e5
afni1011 <afniana17@gmail.com>
2026-09-26 13:45:25 +0700

    feat: create company CRUD API

--- apps/api/src/controllers/company.controller.js
+++ apps/api/src/controllers/company.controller.js
@@ -23,6 +23,7 @@ const getAllCompanies = (req, res) => {
 // GET company berdasarkan id
 const getCompanyById = (req, res) => {
     const id = parseInt(req.params.id, 10);
+
     const company = companies.find(
         (item) => item.id === id
     );
@@ -36,20 +37,72 @@ const getCompanyById = (req, res) => {
     }
 };
 
+// POST Company
 const postAllCompany = (req, res) => {
-    const {name, industry, location, website} = req.body;
+    const { name, industry, location, website } = req.body;
+
     const newCompany = {
         id: companies.length + 1,
         name,
         industry,
         location,
         website
-    }
+    };
+
     companies.push(newCompany);
+
     res.status(201).json(newCompany);
 };
+
+// PUT Company berdasarkan id
+const putCompanyById = (req, res) => {
+    const id = parseInt(req.params.id, 10);
+
+    const company = companies.find(
+        (item) => item.id === id
+    );
+
+    const { name, industry, location, website } = req.body;
+
+    if (company) {
+        company.name = name;
+        company.industry = industry;
+        company.location = location;
+        company.website = website;
+
+        res.json(company);
+    } else {
+        res.status(404).json({
+            error: "Company Not Found"
+        });
+    }
+};
+
+// DELETE Company berdasarkan id
+const delCompanyById = (req, res) => {
+    const id = parseInt(req.params.id, 10);
+
+    const companyIndex = companies.findIndex(
+        (item) => item.id === id
+    );
+
+    if (companyIndex !== -1) {
+        companies.splice(companyIndex, 1);
+
+        res.status(200).json({
+            message: "Company deleted successfully"
+        });
+    } else {
+        res.status(404).json({
+            error: "Company Not Found"
+        });
+    }
+};
+
 module.exports = {
     getAllCompanies,
     getCompanyById,
-    postAllCompany
+    postAllCompany,
+    putCompanyById,
+    delCompanyById
 };
--- apps/api/src/routes/company.routes.js
+++ apps/api/src/routes/company.routes.js
@@ -3,19 +3,16 @@ const router = express.Router();
 
 const {
     getAllCompanies,
-    getCompanyById, postAllCompany
+    getCompanyById,
+    postAllCompany,
+    putCompanyById,
+    delCompanyById,
 } = require("../controllers/company.controller");
 
-// GET semua company
-// /api/companies
 router.get("/", getAllCompanies);
-
-// GET company berdasarkan id
-// /api/companies/:id
 router.get("/:id", getCompanyById);
-
-// POST company
-// /api/companies
 router.post("/", postAllCompany);
+router.put("/:id", putCompanyById);
+router.delete("/:id", delCompanyById);
 
 module.exports = router;
